8434e61fc525b4a1860a68225f82d2d04a0eb997
mahdiASC <mahdi@allstarcode.org>
2019-03-23 23:38:18 -0400

    deployment - not well done

---
+++ precache-manifest.538a06e15f1ed80db92b45067de6d6bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79d7edcb514faefa5079",
+    "url": "/silly_react_quiz/static/js/runtime~main.6ee7112d.js"
+  },
+  {
+    "revision": "8e580f19c657cb3a7c5f",
+    "url": "/silly_react_quiz/static/js/main.f43e0c4e.chunk.js"
+  },
+  {
+    "revision": "0a0cbda9e1ecdadf78d2",
+    "url": "/silly_react_quiz/static/js/2.4df385cd.chunk.js"
+  },
+  {
+    "revision": "8e580f19c657cb3a7c5f",
+    "url": "/silly_react_quiz/static/css/main.b5612cb8.chunk.css"
+  },
+  {
+    "revision": "25fd27ebd1386ce2b00eb497396d70c0",
+    "url": "/silly_react_quiz/index.html"
+  }
+];
